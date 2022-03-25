832d6687e3f4e94816255ca2d201c76e74dd7cff
Hridoyshom <hshom827@gmail.com>
2022-03-25 21:58:52 +0600

    data fetched

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return
+  const [cookies, setCookies] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setCookies(data));
+  }, []);
+
+  return (
+    <div>
+      <h1 className='new'>choose cookie</h1>
+      {cookies.map((cookie) => console.log(cookie.name))}
+    </div>
+  );
 
 
 }
